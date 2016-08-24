1608714409c09ca54166893a432c42591ced4258
Mobolaji O. David <davidmo@colorado.edu>
2016-08-24 14:40:16 -0700

    create program that asks questions and gets user input from employee

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name = gets.chomp.to_s
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "What year were you born?"
+birth_year = gets.chomp.to_i
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y / N)"
+garlic = gets.chomp.to_s
+puts "Would you like to enroll in the company's health insurance (Y / N)?"
+health =gets.chomp.to_s
