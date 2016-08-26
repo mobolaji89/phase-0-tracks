569b3d3751eee708085fcf449e0498ce5a584b48
Mobolaji David <davidmo@colorado.edu>
2016-08-26 15:39:35 -0700

    Update vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,7 @@
+puts "How many employees will be processed?"
+employees = gets.chomp.to_i
+for i in 1..employees do
+
 puts "What is your name?"
 name = gets.chomp.to_s
 puts "How old are you?"
@@ -9,6 +13,43 @@ garlic = gets.chomp.to_s
 puts "Would you like to enroll in the company's health insurance (Y / N)?"
 health =gets.chomp.to_s
 
-if (age <= 100 && birth_year >= 1916) && (garlic = "Y" || health = "Y")
+if name == "Drake Cula" || name == "Tu Fang"
+name = true
+else
+name = false
+end
+
+current_year = 2016
+age_check = current_year-birth_year
+if age == age_check
+	age = true
+else
+	age = false
+end
+
+if garlic == "Y"
+	garlic = true
+elsif garlic == "N" 
+	garlic = false
+end
+
+if health == "Y"
+	health = true
+elsif garlic == "N"
+	health = false
+end
+
+
+if name == false && age == true && (garlic == true || health == true)
 	puts "Probably not a vampire."
+elsif (name == false && (age == false && garlic == false && health == true)) || (name == false && (age == false && garlic == true && health == false))
+	puts "Probably a vampire."
+elsif name == false && (age == false && garlic == false && health == false)
+	puts "Almost certainly a vampire."
+elsif name == true && (age == true || age == false) && (garlic == true || garlic == false) && (health == true || health == false)
+	puts "Definitely a vampire."
+else
+	puts "Results inconclusive."
+end
+
 end
