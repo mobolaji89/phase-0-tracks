3fc41addd9bab982976e09fd955240b6b0a81af1
Mobolaji O. David <davidmo@colorado.edu>
2016-09-08 15:54:44 -0700

    add class design for puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+##class design
+# declare class puppy
+
+#practice
+class Puppy
+end
+
+##under the class
+
+# method for puppy to speak(woof)
+# method for puppy to eat
+# method for puppy to sleep
+# These are a few examples
