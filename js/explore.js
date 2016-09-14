d66a3dd4be8c6120839b363f743d55bebf75fd8c
Mobolaji O. David <davidmo@colorado.edu>
2016-09-14 09:22:55 -0700

    create test.js for node installation, and explore.js to create reverse string function

---
+++ js/explore.js
@@ -0,0 +1,19 @@
+//create reverse function that takes a string passed in as an argument
+//the function will create a new string, and somehow insert the index values of the old string in reverse order
+//use a loop, to iterate through all of the indexes of the string
+//I likely will want to loop through the length of the word
+//I feel the easiest method would be to loop downward using the '--' operator and print the last index first, and so on.
+//print word after loop has finished iterating
+//I'm sure this is one way, another method could be to create a sorting function.
+
+
+function reverse(string) {
+  var x = '';
+  for (var i = string.length - 1; i >= 0; i--)
+    x += string[i];
+  return x;
+}
+
+if (1 == 1){
+console.log(reverse('hello'));
+}
