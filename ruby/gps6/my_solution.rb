ebfa893d07e44ef40bbd13e75d47112d19bdd9d0
Mobolaji O. David <davidmo@colorado.edu>
2016-09-15 23:06:01 -0700

    refactor predicted_deaths and speed of spread methods for release 7

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -19,6 +19,8 @@ class VirusPredictor
     @state = state_of_origin
     @population = population
     @population_density = population_density
+    @speed = 0
+    @number_of_deaths
   end
 
 
@@ -33,40 +35,40 @@ class VirusPredictor
 
   def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+    @number_of_deaths = if @population_density >= 200
+      (@population * 0.4).floor
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      (@population * 0.3).floor
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      (@population * 0.2).floor
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      (@population * 0.1).floor
     else
-      number_of_deaths = (@population * 0.05).floor
+      (@population * 0.05).floor
     end
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+    print "#{@state} will lose #{@number_of_deaths} people in this outbreak"
 
   end
 
   def speed_of_spread
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
 
-    if @population_density >= 200
-      speed += 0.5
+
+    @speed = if @population_density >= 200
+      0.5
     elsif @population_density >= 150
-      speed += 1
+      1
     elsif @population_density >= 100
-      speed += 1.5
+      1.5
     elsif @population_density >= 50
-      speed += 2
+      2
     else
-      speed += 2.5
+      2.5
     end
 
-    puts " and will spread across the state in #{speed} months.\n\n"
+    puts " and will spread across the state in #{@speed} months.\n\n"
 
   end
 
