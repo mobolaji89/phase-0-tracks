2a0b8707a4c48c790330b6b064d17d746068f47f
Mobolaji O. David <davidmo@colorado.edu>
2016-09-16 23:37:38 -0700

    add pseudocode, and create an array for release 0

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+//release0 - write a function that takes an array of words or phrases and returns the longest word or phrase in the array
+
+//define a function and pass it an array as an argument
+//iterate through each index in the array (in the example we have strings)
+//sort through each index, by swapping indexes based on string length
+//move the largest string to one end of the array
+//print that string by accessing the index value
+
+var word_arr = ["abc", "a", "abcd", "abcde", "ab"];
+//console.log(arr);
+
