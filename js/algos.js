c1788231b62faf680da38cf823d853a8de58ea79
Mobolaji O. David <davidmo@colorado.edu>
2016-09-17 15:20:02 -0700

    add pseudocode for release 1

--- js/algos.js
+++ js/algos.js
@@ -22,4 +22,19 @@ function longest_string(arr) {
 return arr[0];
 }
 
+//write a function that takes two objects and checks to see if the objects share at least one key-value pair.
+//If we called your function with {name: "Steven", age: 54} and {name: "Tamir", age: 54}, the function would return true, because at least one key-value pair matches between the two objects. If no pairs match (and keep in mind that the two objects may not even have any of the same keys), the function should return false.
+
+//define a function and pass in two (key-value) objects as arguments
+//iterate through each of the two data structures
+  //start with one for loop to iterate through the first object
+  //then do a nested for loop to iterate through the second object
+//IF one key value pair matches another key value pair
+  //return true
+//ELSE return false
+//print result
+
+
+
+//calling release 0
 console.log(longest_string(word_arr))
