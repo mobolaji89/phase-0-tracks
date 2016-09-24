ac46127f365a118edef6c70a1cb2e1ea9c0aac7f
Mobolaji O. David <davidmo@colorado.edu>
2016-09-23 20:12:28 -0700

    revise driver code syntax for 5.2 repeat

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,3 +1,50 @@
+#create am empty
+#puts each question for the user
+  #after each question, get user input and store the appropriate key in the array, with the value(user-input)
+ #for boolean question, use a if conditional statement, and set the value to true IF Yes, ELSE set value to false IF No, ELSE set value to unknown if neither.
+#
+
+
+app = {}
+
+puts "What is your name?"
+  app[:name] = gets.chomp.to_s
+puts "What is your age?"
+  app[:age] = gets.chomp.to_i
+puts "How many children do you have?"
+  app[:children] = gets.chomp.to_i
+puts "What is your decor theme?"
+  app[:decor] = gets.chomp.to_s
+puts "Will this be a temporary design? (Type: Y or N)"
+  app[:temp] = gets.chomp.to_s
+    if app[:temp] == 'Y'
+      app[:temp] = true
+    elsif app[:temp] == 'N'
+      app[:temp] = false
+    else
+      app[:temp] = "unknown"
+    end
+
+#print Hash    
+p app
+
+#update key
+if app[:temp] == "unknown"
+  puts "Your temporary design answer is unknown. Please try again."
+  puts "Will this be a temporary design? (Type: Y or N)"
+  temp = gets.chomp.to_s
+  app[:temp] = gets.chomp.to_s
+  if app[:temp] == 'Y'
+    app[:temp] = true
+  elsif app[:temp] == 'N'
+    app[:temp] = false
+  else
+    app[:temp] = "unknown"
+  end
+end
+
+=begin
+
 #Pseudocode
 
 #1) Ask interior designer to enter details of a given client (multiple questions), and get user input for each question.
@@ -8,6 +55,7 @@
 
 
 #Steps 1 and 2 - Ask questions, get user input, and assign input to hash values.
+
 puts "What is your name?"
     name = gets.chomp.to_s
 puts "What is your age?"
@@ -52,3 +100,4 @@ if app[:temp] == "unknown"
 end
 
 p app
+=end
