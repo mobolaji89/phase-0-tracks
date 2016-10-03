443e3e3ba9fa6f834c73809e728c73bfd1202e10
Mobolaji O. David <davidmo@colorado.edu>
2016-10-02 23:00:41 -0700

    add methods and test for release 0 - 6.2 redo

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,40 @@
+#6.2 repeat - Coyotes 2016
+class Puppy
+  def initialize
+  	puts "initialize new puppy instance!"
+  end 		
+
+  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy 
+  end
+  
+  def speak(num)
+  	num.times do 
+  	  puts "Woof!"
+  	  end
+    num
+  	end
+    
+  def roll_over 
+  	puts "*rolls over*"
+  end
+  
+  def jump(num)
+    puts "*jumps #{num} inches from the ground*"
+    num
+  end
+end
+
+puppy = Puppy.new
+puppy.fetch('stick')
+puppy.speak(3)
+puppy.roll_over
+puppy.jump(10)
+
+
+#Coyotes 2016
+=begin
 class Puppy
 
   def initialize
@@ -83,8 +120,7 @@ end
 shake.each do |key, value| 
 	puts "#{value}"
 end 
-
-
+=end
 
 
 
