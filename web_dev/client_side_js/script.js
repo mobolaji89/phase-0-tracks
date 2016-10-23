7b96e06e91d57926b705f1b9b427f778c72141c3
Mobolaji David <davidmo@colorado.edu>
2016-10-22 23:36:04 -0700

    create event listener for image border function

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,4 +1,9 @@
 console.log("The script is running!")
 
+function addGoldBorder(event) {
+  //var photo = document.getElementById("cookies-cream-photo");
+  event.target.style.border = "5px solid gold";
+}
+
 var photo = document.getElementById("cookies-cream-photo");
-photo.style.border = "5px solid gold";
+photo.addEventListener("click", addGoldBorder);
